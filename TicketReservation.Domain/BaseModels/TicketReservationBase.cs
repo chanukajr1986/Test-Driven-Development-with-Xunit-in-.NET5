a11d5b7bd22894f5f7933249e136d57c2990101b
chanukajr <chanukajr@gmail.com>
2025-05-26 19:05:09 +0530

    setting empty string

--- TicketReservation.Domain/BaseModels/TicketReservationBase.cs
+++ TicketReservation.Domain/BaseModels/TicketReservationBase.cs
@@ -9,12 +9,12 @@ namespace TicketReservation.Domain.BaseModels
 
         [Required]
         [StringLength(80)]
-        public string FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
 
         [Required]
         [StringLength(80)]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         public DateTime Date { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
